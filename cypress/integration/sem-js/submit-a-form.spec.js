6b7dc3adbaefc52a229c592e43213f4b5cd27652
Jeff Sallans <jsallans@xby2.com>
2018-08-13 13:03:30 -0400

    Add Katalon examples, add cypress demo

---
+++ cypress/integration/sem-js/submit-a-form.spec.js
@@ -0,0 +1,47 @@
+
+describe('Submitting a form', () => {
+    it.only('displays a confirmation message', () => {
+        // https://on.cypress.io/submit
+
+        // Navigate to the webpage
+        cy.visit('https://example.cypress.io/commands/actions');
+        
+        // Select the coupon field
+        cy.get('.action-form')
+            .find('[type="text"]')
+            .type('HALFOFF');
+        // Submit the form
+        cy.get('.action-form')
+            .submit();
+        // Check the result
+        cy.get('.action-form + p')
+            .should('contain', 'Your form has been submitted!');
+    });
+
+    it('fills out checkboxes correctly', () => {
+        // https://on.cypress.io/check
+
+        // By default, .check() will check all
+        // matching checkbox or radio elements in succession, one after another
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]')
+            .check().should('be.checked')
+
+        cy.get('.action-radios [type="radio"]').not('[disabled]')
+            .check().should('be.checked')
+
+        // .check() accepts a value argument
+        cy.get('.action-radios [type="radio"]')
+            .check('radio1').should('be.checked')
+
+        // .check() accepts an array of values
+        cy.get('.action-multiple-checkboxes [type="checkbox"]')
+            .check(['checkbox1', 'checkbox2']).should('be.checked')
+
+        // Ignore error checking prior to checking
+        cy.get('.action-checkboxes [disabled]')
+            .check({ force: true }).should('be.checked')
+
+        cy.get('.action-radios [type="radio"]')
+            .check('radio3', { force: true }).should('be.checked')
+        })
+});
---
+++ katalon/SubmitAForm.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class SubmitAForm
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheSubmitAFormTest()
+        {
+            driver.Navigate().GoToUrl("https://example.cypress.io/commands/actions");
+            driver.FindElement(By.Id("couponCode1")).Click();
+            driver.FindElement(By.Id("couponCode1")).Clear();
+            driver.FindElement(By.Id("couponCode1")).SendKeys("Test");
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Coupon Code'])[1]/following::button[1]")).Click();
+            Assert.AreEqual("Your form has been submitted!", driver.FindElement(By.CssSelector(".action-form + p")).Text);
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
